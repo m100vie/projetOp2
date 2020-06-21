365dd2f1767416f7ee7be1648f8cd03322954eb6
m100vie <msanvee@gmail.com>
2020-06-21 16:05:43 +0200

    changement de nom branche1 par draft

--- OCp2/src/other/Mymap.java
+++ OCp2/src/other/Mymap.java
@@ -25,15 +25,6 @@ public class Mymap {
 			}
 		}
 
-//		for(int i = 0 ; i < file.size() ; i++)	{
-//			
-//			 if(map.containsKey(file.get(i)))	{  
-//				 map.replace(file.get(i),map.get(file.get(i))+1);
-//			 }
-//			 else	{ 
-//				 map.put(file.get(i),1);
-//			 }
-//		}
 		sortedMap = new TreeMap<String, Integer>(map);
 		Set<Entry<String, Integer>> set2 = sortedMap.entrySet();
 		Iterator<Entry<String, Integer>> iterator2 = set2.iterator();
